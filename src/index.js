5243d8a07f94a0dfe1df98822ffd017847e6c295
Harsh Tiwari <harshtiwari343@email.com>
2024-10-09 12:32:20 +0530

    Updated src folder

--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom/client'; // Include ReactDOM module
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root')); // take id root from index.html file
+let username = "Programming with Harsh";
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  username
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
