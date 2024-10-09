07983e825e23805b3ff24bec96fcaaa847896f59
Harsh Tiwari <harshtiwari343@email.com>
2024-10-09 12:57:01 +0530

    Without JSX, With JSX and Expression

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import ReactDOM from 'react-dom/client'; // Include ReactDOM module
-
+import React from 'react';
 const root = ReactDOM.createRoot(document.getElementById('root')); // take id root from index.html file
+
 let username = "Programming with Harsh";
+const flowers = ["Rose", "Dahlia", "Magnolia", "Tulip", "Daisy"];
+
+// Without JSX
+const element = React.createElement("h1", {}, "Programming with Harsh");
+const element2 = React.createElement("ul",
+  {},
+  flowers.map((item, index) => React.createElement("li", { key: index }, item))
+)
+
+// With JSX, Directly write HTML code in Javascript
+const element3 = <h1>Programming with Harsh 2+2 is {2 + 2}</h1>;
+const element4 = <ul>
+  {
+    flowers.map((item, index) => <li key={index}>{item}</li>)
+  }
+</ul>
+
 root.render(
-  username
+  element4
 );
 
