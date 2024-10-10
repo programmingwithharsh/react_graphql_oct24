f68cbf4e39b7b0c8e253b03554668add625392bb
Harsh Tiwari <harshtiwari343@email.com>
2024-10-10 11:42:03 +0530

    State in class component

--- src/Main.js
+++ src/Main.js
@@ -6,20 +6,20 @@ import Hello from "./component/Hello";
 import Title from "./Title";
 
 export default class Main extends React.Component {
-    constructor() { // Class component Lifecycle
+    constructor() { // Class component lifecycle
         super();
-        console.log("Constructor lifecyle - 1");
+        console.log("Main Component Constructor lifecycle - 1");
     }
 
     render() { // Class component lifecycle, render view
-        console.log("Render lifecyle - 2");
-        return <div>
+        console.log("Main Component Render lifecycle - 2");
+        return <>
             <h1>Main Class Component</h1>
             <Welcome />
             <AddProduct />
             <ProductList />
             <Hello />
             <Title />
-        </div>
+        </>
     }
 }
--- src/Welcome.js
+++ src/Welcome.js
@@ -1,7 +1,44 @@
 import React from "react";
 
 export default class Welcome extends React.Component {
-    render() {
-        return <h1>Welcome Class Component</h1>
+
+    constructor() { // lifecycle method
+        super();
+        this.state = { // define state, state is an object
+            username: "Atul",
+            address: "Mumbai"
+        }
+        console.log("Welcome Component Constructor lifecycle - 1");
+        this.updateUsername = this.updateUsername.bind(this);
+    }
+
+    updateUsername() { // normal method
+        this.setState({ // to update state, we use this.setState, whenever state update component rerender
+            username: "Sandeep"
+        })
+    }
+
+    updateAddress = () => { // arrow method
+        this.setState({
+            address: "Delhi"
+        })
+    }
+
+    updateAll = () => { // method
+        this.setState({
+            username: "Sandeep",
+            address: "Delhi"
+        })
+    }
+
+    render() { // lifecycle method
+        console.log("Welcome Component Render lifecycle - 2");
+        return <>
+            <h1>Welcome Class Component</h1>
+            <h2>State username is {this.state.username} and address is {this.state.address}</h2>
+            <button onClick={this.updateAll}>Update All</button>
+            <button onClick={this.updateUsername}>Update Username</button>
+            <button onClick={this.updateAddress}>Update Address</button>
+        </>
     }
 }
